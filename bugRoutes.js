00eb879ef24fbb3aeca4bf1d530c4c1bb2ba1062
Charanlokesh22 <charanlokesh2918@gmail.com>
2025-08-06 20:42:14 +0530

    Create bugRoutes.js

---
+++ bugRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const {
+  createBug,
+  getAllBugs,
+  updateBugStatus,
+} = require('../controllers/bugController');
+
+router.post('/bugs', createBug);
+router.get('/bugs', getAllBugs);
+router.put('/bugs/:id/status', updateBugStatus);
+
+module.exports = router;
