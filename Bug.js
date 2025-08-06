064519449095c7c5e2c6dd3d26e9b9df17ec3a7b
Charanlokesh22 <charanlokesh2918@gmail.com>
2025-08-06 20:41:43 +0530

    Create Bug.js

---
+++ Bug.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const bugSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  status: {
+    type: String,
+    enum: ['Open', 'In Progress', 'Resolved'],
+    default: 'Open',
+  },
+  reportedBy: String,
+  assignedTo: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Bug', bugSchema);
