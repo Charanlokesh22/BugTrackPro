126783f2f7ef4e8ead8b1440ae56c9e039744c8c
Charanlokesh22 <charanlokesh2918@gmail.com>
2025-08-06 20:42:36 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./config/db');
+const bugRoutes = require('./routes/bugRoutes');
+const cors = require('cors');
+
+connectDB();
+app.use(cors());
+app.use(express.json());
+app.use('/api', bugRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
